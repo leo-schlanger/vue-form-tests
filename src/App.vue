c0d3de5998d458570de6581a85bf1ab4cb84cff4
leo-schlanger <leoschlanger@gmail.com>
2021-04-15 16:29:58 -0300

    input component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="new-user-form">
+      <Input placeholder="Nome" />
+      <Input placeholder="E-mail" />
+      <Input placeholder="Idade" />
+      <button type="submit">Salvar</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Input from './components/Input.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Input
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 * {
   margin: 0;
   padding: 0;
@@ -27,6 +32,11 @@ export default {
   font: 400 1rem "Roboto", sans-serif;
   width: 100vw;
   height: 100vh;
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -42,5 +52,32 @@ a {
   color: inherit;
   text-decoration: none;
   cursor: pointer;
+}
+
+.new-user-form {
+  width: 100%;
+  max-width: 760px;
+  margin-top: 80px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  align-self: center;
+
+  button {
+    width: 15%;
+    height: 32px;
+    font-size: 16px;
+    background-color: green;
+    color: #e1e1e6;
+    border: 0;
+    border-radius: 8px;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.7);
+    }
+  }
 }
 </style>
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
-
-  </div>
+    <div class="input-style">
+      <input value="" v-bind:placeholder="this.placeholder">
+    </div>
 </template>
 
 <script>
   export default {
-    
+    name: 'Input',
+    props: {
+      placeholder: String,
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+.input-style {
+  input { 
+    padding: 8px;
+    height: 36px;
 
+    border: 1px solid;  
+    border-radius: 4px;
+  }
+
+  & + div.input-style {
+    margin-top: 16x;
+  }
+}
 </style>
